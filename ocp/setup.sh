9635a390a410a29f7fb60e828178a973692e5941
Asier Cidon <asier.cidon@gmail.com>
2023-04-22 11:10:17 +0200

    Added some change in roles

--- ocp/setup.sh
+++ ocp/setup.sh
@@ -1,12 +1,73 @@
 ##
-# 
+# Script to setup the ENV
 ##
 #!/bin/bash
 
-oc apply -f subscription.yaml
+USERS="arch01-developer
+arch01-operator
+arch02-developer
+arch02-operator
+"
+GROUPSS="arch01-developers
+arch01-operators
+arch02-developers
+arch02-operators
+"
 
+##
+# Adding user to htpasswd
+##
+htpasswd -c -b users.htpasswd admin password
+for i in $USERS
+do
+  htpasswd -b users.htpasswd $i $i
+done
+
+##
+# Creating groups 
+##
+for i in $GROUPSS
+do
+  oc adm groups new $i
+done
+
+##
+# Adding users to groups 
+##
+for i in $USERS
+do
+  oc adm groups add-users ${i}s $i
+done
+
+##
+# Creating htpasswd file in Openshift
+##
+oc delete secret lab-users -n openshift-config
+oc create secret generic lab-users --from-file=htpasswd=users.htpasswd -n openshift-config
+
+##
+# Configuring OAuth to authenticate users via htpasswd
+##
+cat <<EOF > oauth.yaml
+apiVersion: config.openshift.io/v1
+kind: OAuth
+metadata:
+  name: cluster
+spec:
+  identityProviders:
+  - htpasswd:
+      fileData:
+        name: lab-users
+    mappingMethod: claim
+    name: lab-users
+    type: HTPasswd
+EOF
+cat oauth.yaml | oc apply -f -
+
+##
+# Install ArgoCD Operator and Instance
+##
+oc apply -f ocp/subscription.yaml
 sleep 60
-
 oc new-project argocd
-
-oc apply -f argocd.yaml -n argocd
+oc apply -f ocp/argocd.yaml -n argocd
